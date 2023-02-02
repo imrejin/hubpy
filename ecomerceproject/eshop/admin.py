fda57ff9b5fe1080f8ba765ec95071a3001944b2
Rejin R S <rejinrsmulavna@gmail.com>
2023-02-02 19:37:10 +0530

    added

---
+++ ecomerceproject/eshop/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Category, Product
+
+
+class categoryadmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+
+admin.site.register(Category, categoryadmin)
+
+
+class productadmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'stock', 'available', 'created', 'updated']
+    list_editable = ['price', 'stock', 'available']
+    prepopulated_fields = {'slug': ('name',)}
+
+
+admin.site.register(Product, productadmin)
---
+++ ecomerceproject/search_app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from eshop.models import Product
+from django.db.models import Q
+from django.http import HttpResponse
+
+def searchResult(request):
+    products=None
+    query=None
+    if 'q' in request.GET:
+        query=request.GET.get('q')
+        products=Product.objects.all().filter(Q(name__contains=query) | Q(description__contains=query))
+        return render(request,'search.html',{'query':query,'products':products})
